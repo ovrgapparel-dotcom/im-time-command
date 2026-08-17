8160e595c37bdb0dd363108c466e15c4dce187f2
Virgile Obeo <ovrg.apparel@gmail.com>
2026-08-17 18:00:48 -0400

    Initial commit: iM Time Command v3 Global Entrepreneur Suite

---
+++ server.js
@@ -0,0 +1,44 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3000;
+const PUBLIC_DIR = __dirname;
+
+const MIME_TYPES = {
+  '.html': 'text/html; charset=utf-8',
+  '.css': 'text/css',
+  '.js': 'application/javascript',
+  '.png': 'image/png',
+  '.jpg': 'image/jpeg',
+  '.json': 'application/json',
+  '.svg': 'image/svg+xml'
+};
+
+const server = http.createServer((req, res) => {
+  let reqUrl = req.url.split('?')[0];
+  if (reqUrl === '/') reqUrl = '/index.html';
+  
+  const filePath = path.join(PUBLIC_DIR, reqUrl);
+  const ext = path.extname(filePath).toLowerCase();
+  const contentType = MIME_TYPES[ext] || 'application/octet-stream';
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('404 Not Found');
+      } else {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('500 Internal Server Error');
+      }
+    } else {
+      res.writeHead(200, { 'Content-Type': contentType });
+      res.end(content);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`iM Time Command server running at http://localhost:${PORT}`);
+});
